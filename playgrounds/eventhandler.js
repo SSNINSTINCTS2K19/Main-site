f7e3c76174287dae2f65b0dc76213c1ba2baf9d0
nikamanthab <nitinnikamanth@gmail.com>
2019-02-07 22:12:56 +0530

    col gen

--- playgrounds/eventhandler.js
+++ playgrounds/eventhandler.js
@@ -1,17 +1,23 @@
-let design = document.querySelector('#design');
+let design = document.querySelector('#demo01');
 let lop = document.querySelector('lop');
 
 // document.querySelector("h1").style.display="block";
 
-design.addEventListener('click',()=>{
+design.addEventListener('click', () => {
     fetch("./designclub.json").then(response => response.json())
-    .then(
-        d=>{console.log(d)
-        document.querySelector("#clubtitle").textContent = d.club
-        d.events.forEach(element => {
-            var x = document.createElement('li');
-            x.textContent=element.title;
-            document.querySelector("#eventlist").appendChild(x);
-        });
-        })    
+        .then(
+            
+            d => {
+                console.log(d)
+                document.querySelector("#eventlist").innerHTML="";
+                document.querySelector("#clubtitle").textContent = d.club
+                d.events.forEach(element => {
+                    var x = document.createElement('li');
+                    x.textContent = element.title;
+                    document.querySelector("#eventlist").appendChild(x);
+
+                });
+                console.log(d.events[0].description);
+                document.querySelector("#description").textContent = d.events[0].description;
+            })
 })
