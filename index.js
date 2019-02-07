194822370bf89ecf9605575b6928379d2c61ebeb
AK5123 <kanduarul@gmail.com>
2019-02-08 01:41:58 +0530

    Starevents

---
+++ index.js
@@ -0,0 +1,19 @@
+(function () {
+    document.querySelectorAll(".stars").forEach(e=>{
+        e.addEventListener("click",listen);
+    });
+  function listen(event) {
+      document.querySelector("div").innerHTML="";
+      infor(event.target.textContent);
+    }
+  function infor(data){
+      fetch(data+".json").then(function (response) { return response.json(); }).then(function (e) {
+           console.log(e.head);
+           e.events.forEach((d)=>{
+               document.querySelector("div").innerHTML += "<p>" + d + "</p>"
+           });
+          
+        })  
+    }
+
+})();
