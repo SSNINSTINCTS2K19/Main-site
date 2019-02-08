40dbc3ff8c2966a588618d10d756c70e2885031b
nikamanthab <nitinnikamanth@gmail.com>
2019-02-08 15:13:30 +0530

    event injection

--- playgrounds/eventhandler.js
+++ playgrounds/eventhandler.js
@@ -8,37 +8,36 @@ design.addEventListener('click', () => {
     fetch("json/EnglishLiteraryClub.json").then(response => response.json())
         .then(
             d => {
-                   console.log(d)
-                   document.querySelector("#eventlist").innerHTML = "";
-                   document.querySelector("#clubtitle").textContent = d.name;
-                    for (var i = 0; i < d.events.length; i++) {
-                      eventsbarlist.push(d.events[i].title);
-                    } 
-                    eventsbarlist.forEach((data, i) => {
-                        let ele = document.createElement("li");
-                         ele.textContent = data;
-                         ele.addEventListener("click", function () {
-                             
-                         });
-                         console.log(    document.querySelector("#eventlist"));
-                        document.querySelector("#eventlist").appendChild(ele);
+                console.log(d)
+                document.querySelector("#eventlist").innerHTML = "";
+                document.querySelector("#clubtitle").textContent = d.name;
+                for (var i = 0; i < d.events.length; i++) {
+                    eventsbarlist.push(d.events[i].title);
+                }
+                eventsbarlist.forEach((data, i) => {
+                    let ele = document.createElement("li");
+                    ele.textContent = data;
+                    ele.addEventListener("click", function () {
+
                     });
-                    console.log(d.events[0]);
-                    for(var property in d.events[0]){
-                        var a;
-                        if(property=="title"){
-                             console.log(55);
-                             a=document.createElement("h2");
-                           
-                        }
-                        else{
-                           a=document.createElement("p");
-                        }
-                        a.innerHTML=d.events[0][property];
-                        console.log(d.events[0][property]);
-                        console.log(document.querySelector("#eventcontents"));
-                        document.querySelector("#eventcontents").appendChild(a);
+                    console.log(document.querySelector("#eventlist"));
+                    document.querySelector("#eventlist").appendChild(ele);
+                });
+                console.log(d.events[0]);
+                for (var property in d.events[0]) {
+                    var a;
+                    if (property == "title") {
+                        console.log(55);
+                        a = document.createElement("h2");
+
+                    } else {
+                        a = document.createElement("p");
                     }
+                    a.innerHTML = d.events[0][property];
+                    console.log(d.events[0][property]);
+                    console.log(document.querySelector("#eventcontents"));
+                    document.querySelector("#eventcontents").appendChild(a);
+                }
 
 
 
