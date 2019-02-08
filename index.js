c77b37747c7e8b35c6e898ebea2086e25776e12f
AK5123 <kanduarul@gmail.com>
2019-02-08 12:11:20 +0530

    localstoragedone

--- index.js
+++ index.js
@@ -10,7 +10,6 @@
       infor(event.target.textContent);
     }
   function infor(data){
-<<<<<<< HEAD
       if(localStorage.getItem(data)){
           console.log(199);
           let e = JSON.parse(localStorage.getItem(data));
@@ -19,9 +18,7 @@
           });
       }
       else{
-=======
     //   local storage 
->>>>>>> e7089406ae0ba0e12052f3b737a4840ddfdbeb81
       fetch(data+".json").then(function (response) { return response.json(); }).then(function (e) {
           localStorage.setItem(e.head.toLowerCase(),JSON.stringify(e));
            console.log(e.head);
