f9af488b4950b858b404fc295941dfb308067479
AK5123 <kanduarul@gmail.com>
2019-02-08 12:05:20 +0530

    localstorage

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//localStorage.removeItem("SFC");
+//localStorage.removeItem("ELC");
+
 (function () {
     document.querySelectorAll(".stars").forEach(e=>{
         e.addEventListener("click",listen);
@@ -7,13 +10,21 @@
       infor(event.target.textContent);
     }
   function infor(data){
+      if(localStorage.getItem(data)){
+          console.log(199);
+          let e = JSON.parse(localStorage.getItem(data));
+          e.events.forEach((d) => {
+              document.querySelector("div").innerHTML += "<p>" + d + "</p>"
+          });
+      }
+      else{
       fetch(data+".json").then(function (response) { return response.json(); }).then(function (e) {
+          localStorage.setItem(e.head.toLowerCase(),JSON.stringify(e));
            console.log(e.head);
            e.events.forEach((d)=>{
                document.querySelector("div").innerHTML += "<p>" + d + "</p>"
-           });
-          
-        })  
+            });
+        });
+     } 
     }
-
 })();
