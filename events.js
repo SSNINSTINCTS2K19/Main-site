b3e54274d4db33cd4cb68573301159dd12f0a96a
mohanpierce99 <paying.rich6990@gmail.com>
2019-02-17 00:15:30 +0530

    Final fix

--- events.js
+++ events.js
@@ -54,14 +54,19 @@ let eventModule = function () {
                 // when window width is <= 320px
                 320: {
                     slidesPerView: 1,
+                    initialSlide:0
+
                 },
                 // when window width is <= 480px
                 480: {
                     slidesPerView: 1,
+                    initialSlide:0
+
                 },
                 // when window width is <= 640px
                 768: {
                     slidesPerView: 2,
+                    initialSlide:0
                 }
             },
             coverflowEffect: {
